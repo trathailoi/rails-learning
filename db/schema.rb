8787837900488bf40c7d538db5861229135a61ce
Trà Thái Lợi <trathailoi@gmail.com>
2022-05-03 23:34:09 +0700

    build: dockerize the app

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_03_073938) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
