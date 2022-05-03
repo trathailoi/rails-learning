2cd8baa6aade546d1ae4a8e093255dc56a60e274
Trà Thái Lợi <trathailoi@gmail.com>
2022-05-03 23:53:53 +0700

    build: git hook for linter with lefthook

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,8 @@ class PostsController < ApplicationController
   end
 
   # GET /posts/1 or /posts/1.json
-  def show; end
+  def show
+  end
 
   # GET /posts/new
   def new
--- scripts/run-tests.bash
+++
@@ -1,8 +0,0 @@
-# #!/usr/bin/env bash
-
-# set -e
-
-# cd "${0%/*}/.."
-
-# echo "Running tests"
-# bundle exec rspec
